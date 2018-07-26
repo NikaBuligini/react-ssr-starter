4e57be072e77d103d63f6cdb40bcd2c0d6acc313
ggogobera <ggogoberishvili@outlook.com>
2018-07-26 15:31:46 +0400

    added flow typed

--- src/redux/actions/counter.js
+++ src/redux/actions/counter.js
@@ -1,11 +1,18 @@
+/** @flow */
+
 /**
  * Counter increment/decrement
  */
 
+export type AlterCounterValueTypes = {
+  type: string,
+  step: number,
+};
+
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
-export function alterCounterValue(type, step) {
+export function alterCounterValue(type: string, step: number) {
   return {
     type,
     step,
