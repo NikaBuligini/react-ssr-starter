e32ce11c75e1e4f91bac0bb6661a86b412750134
goga <ggogobera>
2019-02-09 23:54:18 +0400

    removed everything, even from package.json. goes for new babel v.7. so this is initial commit

--- server/serverUtils/configureStore.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from '../../src/redux/reducers';
-
-export default (preloadedState, extraMiddlewares = []) =>
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk, ...extraMiddlewares));
